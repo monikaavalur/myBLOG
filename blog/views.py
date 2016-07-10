15b98f43c83a06b7772046d39d24539a9c9f9c9b
monika <monika.avalur@gmail.com>
2016-07-10 14:21:25 +0530

    update

--- blog/views.py
+++ blog/views.py
@@ -93,6 +93,9 @@ def posts_list(request):
     page = request.GET.get(page_request_var)
     try:
         queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         queryset = paginator.page(paginator.num_pages)
