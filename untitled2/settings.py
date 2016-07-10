e365026cf0711e2022a48452f0052250f20b261a
monika <monika.avalur@gmail.com>
2016-07-10 16:13:14 +0530

    update

--- untitled2/settings.py
+++ untitled2/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'untitled2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join('templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
