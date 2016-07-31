dfb96faad4b837468b24a7d47ee983ba2c0d0f81
monikaavalur <monika.avalur@gmail.com>
2016-07-31 21:30:10 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -110,21 +110,21 @@ def posts_list(request):
 @login_required
 def posts_update(request, slug=None):
     instance = get_object_or_404(post, slug=slug)
-    form = PostForm(request.POST or None,request.FILES or None,instance=instance)
-    if form.is_valid():
-        if request.user is not instance.user:
-            messages.success(request,"You cannot edit this post")
-        else:
+    if request.user is not instance.user:
+            messages.error(request,"You cannot edit this post")
+    else:
+        form = PostForm(request.POST or None,request.FILES or None,instance=instance)
+        if form.is_valid():
             instance = form.save(commit=False)
             instance.save()
             messages.success(request, "Successfully saved")
-        return HttpResponseRedirect(instance.get_absolute_url())
-    context = {
+            return HttpResponseRedirect(instance.get_absolute_url())
+        context = {
               "title": instance.title,
               "instance": instance,
               "form":form
             }
-    return render(request, "post_form.html", context)
+        return render(request, "post_form.html", context)
 
 @login_required
 def posts_delete(request,slug=None):
